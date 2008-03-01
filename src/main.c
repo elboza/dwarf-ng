94225b9ee5f0486ea1feb3da83bb66ae10cb8dac
Fernando Iazeolla <fernando.iazeolla@gmail.com>
2008-03-01 08:56:34 +0000

    added GNU GPL licence, GPL headers in files, and all misc files (README, INSTALL, COPYING, etc...).

--- src/main.c
+++ src/main.c
@@ -1,3 +1,21 @@
+/*
+ main.c : dwarf's main file.
+
+ (c) 2007-2008 Fernando Iazeolla
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
@@ -60,6 +78,10 @@ void parse_args(int argc,char **argv)
 		switch(c)
 		{
 			case 'i':
+				printf("dwarf Copyright (C) 2007-2008 Fernando Iazeolla\n");
+    				printf("This program comes with ABSOLUTELY NO WARRANTY.\n");
+    				printf("This is free software (GNU GPLv2), and you are welcome to redistribute it\n");
+    				printf("under GNU GPL version 2 (see <http://www.gnu.org/licenses/>).\n");
 				printf("entering shell-interactive mode....testing...\n");
 				shell();
 				strncpy(cmd,"quit;",1024);
