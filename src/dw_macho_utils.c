f933d7383b43b80dc349e6b2d41c2cf46145ff61
Fernando Iazeolla <fernando.iazeolla@gmail.com>
2008-03-12 13:04:10 +0000

    added PE sections reading. added endianess to elf and Mach-O headers.

--- src/dw_macho_utils.c
+++ src/dw_macho_utils.c
@@ -42,7 +42,7 @@
 
 void load_macho_hd()
 {
-	int n_seg,n_sect;
+	int n_seg,n_sect,x,m_ncmds,m_seg_nsects,m_lc_cmd,m_lc_cmdsize;
 	char path[255];
 	struct mach_header *mac;
 	struct load_command *lc;
@@ -50,14 +50,21 @@ void load_macho_hd()
 	struct section *sect;
 	mac=(struct mach_header*)faddr;
 	make_table(NULL,"s",-1);
-	add_s_var("s","magic",TYPE_VAL,&mac->magic);
-	add_s_var("s","cputype",TYPE_VAL,&mac->cputype);
-	add_s_var("s","cpusubtype",TYPE_VAL,&mac->cpusubtype);
-	add_s_var("s","filetype",TYPE_VAL,&mac->filetype);
-	add_s_var("s","ncmds",TYPE_VAL,&mac->ncmds);
-	add_s_var("s","sizeofcmds",TYPE_VAL,&mac->sizeofcmds);
-	add_s_var("s","flags",TYPE_VAL,&mac->flags);
-	if(mac->ncmds>0)
+	x=get_data32(mac->magic);
+	add_s_var("s","magic",TYPE_VAL,&x);
+	x=get_data32(mac->cputype);
+	add_s_var("s","cputype",TYPE_VAL,&x);
+	x=get_data32(mac->cpusubtype);
+	add_s_var("s","cpusubtype",TYPE_VAL,&x);
+	x=get_data32(mac->filetype);
+	add_s_var("s","filetype",TYPE_VAL,&x);
+	m_ncmds=get_data32(mac->ncmds);
+	add_s_var("s","ncmds",TYPE_VAL,&m_ncmds);
+	x=get_data32(mac->sizeofcmds);
+	add_s_var("s","sizeofcmds",TYPE_VAL,&x);
+	x=get_data32(mac->flags);
+	add_s_var("s","flags",TYPE_VAL,&x);
+	if(m_ncmds>0)
 	{
 		make_table(NULL,"lc",mac->ncmds);
 		lc=(struct load_command*)(faddr+sizeof(struct mach_header));
@@ -66,41 +73,63 @@ void load_macho_hd()
 		do
 		{
 			//printf("%d.%d(%x)%d ",n,lc->cmd,lc,lc->cmdsize);
-			switch(lc->cmd)
+			m_lc_cmd=get_data32(lc->cmd);
+			m_lc_cmdsize=get_data32(lc->cmdsize);
+			switch(m_lc_cmd)
 			{
 			case LC_SEGMENT:
 				//printf("LC_SEGMENT\n");
 				seg=(struct segment_command*)lc;
 				sprintf(path,"lc[%d]",n_seg);
-				add_s_var(path,"cmd",TYPE_VAL,&seg->cmd);
-				add_s_var(path,"cmdsize",TYPE_VAL,&seg->cmdsize);
+				x=get_data32(seg->cmd);
+				add_s_var(path,"cmd",TYPE_VAL,&x);
+				x=get_data32(seg->cmdsize);
+				add_s_var(path,"cmdsize",TYPE_VAL,&x);
+				//x=get_data32(seg->segname);
 				add_s_var(path,"segname",TYPE_STRING,&seg->segname);
-				add_s_var(path,"vmaddr",TYPE_VAL,&seg->vmaddr);
-				add_s_var(path,"vmsize",TYPE_VAL,&seg->vmsize);
-				add_s_var(path,"fileoff",TYPE_VAL,&seg->fileoff);
-				add_s_var(path,"filesize",TYPE_VAL,&seg->filesize);
-				add_s_var(path,"maxprot",TYPE_VAL,&seg->maxprot);
-				add_s_var(path,"initprot",TYPE_VAL,&seg->initprot);
-				add_s_var(path,"nsects",TYPE_VAL,&seg->nsects);
-				add_s_var(path,"flags",TYPE_VAL,&seg->flags);
-				if(seg->nsects>0)
+				x=get_data32(seg->vmaddr);
+				add_s_var(path,"vmaddr",TYPE_VAL,&x);
+				x=get_data32(seg->vmsize);
+				add_s_var(path,"vmsize",TYPE_VAL,&x);
+				x=get_data32(seg->fileoff);
+				add_s_var(path,"fileoff",TYPE_VAL,&x);
+				x=get_data32(seg->filesize);
+				add_s_var(path,"filesize",TYPE_VAL,&x);
+				x=get_data32(seg->maxprot);
+				add_s_var(path,"maxprot",TYPE_VAL,&x);
+				x=get_data32(seg->initprot);
+				add_s_var(path,"initprot",TYPE_VAL,&x);
+				m_seg_nsects=get_data32(seg->nsects);
+				add_s_var(path,"nsects",TYPE_VAL,&m_seg_nsects);
+				x=get_data32(seg->flags);
+				add_s_var(path,"flags",TYPE_VAL,&x);
+				if(m_seg_nsects>0)
 				{
-					make_table(path,"sect",seg->nsects);
+					make_table(path,"sect",m_seg_nsects);
 					sect=(struct section*)((char*)seg+sizeof(struct segment_command));
 					n_sect=0;
 					do{
 						sprintf(path,"lc[%d]->sect[%d]",n_seg,n_sect);
 						add_s_var(path,"sectname",TYPE_STRING,&sect->sectname);
 						add_s_var(path,"segname",TYPE_STRING,&sect->segname);
-						add_s_var(path,"addr",TYPE_VAL,&sect->addr);
-						add_s_var(path,"size",TYPE_VAL,&sect->size);
-						add_s_var(path,"offset",TYPE_VAL,&sect->offset);
-						add_s_var(path,"align",TYPE_VAL,&sect->align);
-						add_s_var(path,"reloff",TYPE_VAL,&sect->reloff);
-						add_s_var(path,"nreloc",TYPE_VAL,&sect->nreloc);
-						add_s_var(path,"flags",TYPE_VAL,&sect->flags);
-						add_s_var(path,"reserved1",TYPE_VAL,&sect->reserved1);
-						add_s_var(path,"reserved2",TYPE_VAL,&sect->reserved2);
+						x=get_data32(sect->addr);
+						add_s_var(path,"addr",TYPE_VAL,&x);
+						x=get_data32(sect->size);
+						add_s_var(path,"size",TYPE_VAL,&x);
+						x=get_data32(sect->offset);
+						add_s_var(path,"offset",TYPE_VAL,&x);
+						x=get_data32(sect->align);
+						add_s_var(path,"align",TYPE_VAL,&x);
+						x=get_data32(sect->reloff);
+						add_s_var(path,"reloff",TYPE_VAL,&x);
+						x=get_data32(sect->nreloc);
+						add_s_var(path,"nreloc",TYPE_VAL,&x);
+						x=get_data32(sect->flags);
+						add_s_var(path,"flags",TYPE_VAL,&x);
+						x=get_data32(sect->reserved1);
+						add_s_var(path,"reserved1",TYPE_VAL,&x);
+						x=get_data32(sect->reserved2);
+						add_s_var(path,"reserved2",TYPE_VAL,&x);
 						sect=(struct section*)((char*)sect+sizeof(struct section));
 						n_sect++;
 					}while(n_sect<seg->nsects);
@@ -109,9 +138,9 @@ void load_macho_hd()
 			defaulf:
 				break;
 			}
-			lc=(struct load_command*)((char*)lc+lc->cmdsize);
+			lc=(struct load_command*)((char*)lc+m_lc_cmdsize);
 			n_seg++;
-		}while(n_seg<mac->ncmds);
+		}while(n_seg<m_ncmds);
 	}
 	//make_table(NULL,"lc",4);
 	//make_table("lc[2]","sect",2);
