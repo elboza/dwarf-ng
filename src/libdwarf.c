7e735a1ebd5e1ac0ab69822c832fa91ee35acac6
Fernando Iazeolla <fernando.iazeolla@gmail.com>
2017-07-23 10:52:26 +0200

    fixed nocolors given bug.

--- src/libdwarf.c
+++ src/libdwarf.c
@@ -51,7 +51,7 @@ void dw_init()
 		sprintf(str,"%s/.dwarfrc",envstr);
 		readcfg(str);
 	}
-	if(cfg.colors) set_colors(true);
+	if(cfg.colors) set_colors(true); else set_colors(false);
 }
 void readcfg(char *s)
 {
